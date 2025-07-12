05b73dec3a3a60005508daf1f4d367a4a167f733
NINTCH <135129862+Noo00bdev@users.noreply.github.com>
2025-07-12 12:59:15 +0000

    finishing

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -11,10 +11,28 @@ btn.addEventListener('click', function(){
         const allCountries = data.countries
         const allCity = data.countries.flatMap(countries => countries.cities)
         const allTemples = data.temples
+        const allBeaches = data.beaches
         
-        const result = allCountries.filter(countries => countries.name.toLowerCase() === searchValue);
-        if(result.length > 0){
-            result.forEach(countries => {
+        const countriesTest = allCountries.filter(countries => countries.name.toLowerCase() === searchValue);
+        const cityTest = allCity.filter(cities => cities.name.toLowerCase() === searchValue);
+        let templesTest = [];
+        if (searchValue === "temples" || searchValue === "temple") {
+            templesTest = allTemples;
+        } else {
+            templesTest = allTemples.filter(temples =>
+            temples.name.toLowerCase() === searchValue );
+        }
+
+        let beachesTest = [];
+        if (searchValue === "beaches" || searchValue === "plage") {
+            beachesTest = allBeaches;
+        } else {
+            beachesTest = allBeaches.filter(beaches =>
+            beaches.name.toLowerCase() === searchValue);
+        }
+
+        if(countriesTest.length > 0){
+            countriesTest.forEach(countries => {
                 countries.cities.forEach(cities => {
                 let showDiv = document.createElement('div')
                 showDiv.classList.add('cards')
@@ -38,10 +56,74 @@ btn.addEventListener('click', function(){
             
 
             
-        }else{
-            console.log("Pas trouvé")
+        }else if(cityTest.length > 0){
+            cityTest.forEach(cities => {
+                let showDiv = document.createElement('div')
+                showDiv.classList.add('cards')
+
+                const img = document.createElement('img')
+                img.setAttribute('src', cities.imageUrl)
+
+                const title = document.createElement('p')
+                title.textContent = cities.name
+
+                const description = document.createElement('p')
+                description.textContent = cities.description
+
+                showDiv.appendChild(img)
+                showDiv.appendChild(title)
+                showDiv.appendChild(description)
+                show.appendChild(showDiv)
+
+            })   
+        }else if(templesTest.length > 0){
+            templesTest.forEach(temples => {
+                let showDiv = document.createElement('div')
+                showDiv.classList.add('cards')
+
+                const img = document.createElement('img')
+                img.setAttribute('src', temples.imageUrl)
+
+                const title = document.createElement('p')
+                title.textContent = temples.name
+
+                const description = document.createElement('p')
+                description.textContent = temples.description
+
+                showDiv.appendChild(img)
+                showDiv.appendChild(title)
+                showDiv.appendChild(description)
+                show.appendChild(showDiv)
+
+            })
+        }else if(beachesTest.length > 0){
+            beachesTest.forEach(beaches => {
+                let showDiv = document.createElement('div')
+                showDiv.classList.add('cards')
+
+                const img = document.createElement('img')
+                img.setAttribute('src', beaches.imageUrl)
+
+                const title = document.createElement('p')
+                title.textContent = beaches.name
+
+                const description = document.createElement('p')
+                description.textContent = beaches.description
+
+                showDiv.appendChild(img)
+                showDiv.appendChild(title)
+                showDiv.appendChild(description)
+                show.appendChild(showDiv)
+
+            })
         }
-    })
+        else {
+            // Aucun résultat
+            const notFound = document.createElement('p');
+            notFound.textContent = "❌ Aucun résultat trouvé.";
+            show.appendChild(notFound);
+        }
+    })  
     .catch(error => {
         console.log("Impossible")
     })
